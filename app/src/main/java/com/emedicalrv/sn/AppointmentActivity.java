a100fb5d8d665e464db7fc7ea13be2e75d5b0eee
Ismaila SY <baboyesy10@gmail.com>
2020-11-27 22:35:34 +0000

    Creation entre les Activite de RDV et consultation

--- app/src/main/java/com/emedicalrv/sn/AppointmentActivity.java
+++ app/src/main/java/com/emedicalrv/sn/AppointmentActivity.java
@@ -2,13 +2,42 @@ package com.emedicalrv.sn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AppointmentActivity extends AppCompatActivity {
+    private androidx.cardview.widget.CardView ap_create_rv,ap_list_rv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+
+        ap_create_rv = findViewById(R.id.ap_create_rv);
+        ap_list_rv = findViewById(R.id.ap_list_rv);
+
+        ap_create_rv.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(AppointmentActivity.this, CreateRdvActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        ap_list_rv.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(AppointmentActivity.this, ListRdvActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/emedicalrv/sn/ConsultationActivity.java
+++ app/src/main/java/com/emedicalrv/sn/ConsultationActivity.java
@@ -2,13 +2,40 @@ package com.emedicalrv.sn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConsultationActivity extends AppCompatActivity {
-
+    private androidx.cardview.widget.CardView create_cons,list_cons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultation);
+
+        create_cons = findViewById(R.id.create_cons);
+        list_cons = findViewById(R.id.list_cons);
+
+        create_cons.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(ConsultationActivity.this, CreateConsultActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        list_cons.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(ConsultationActivity.this, ListConsultActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
